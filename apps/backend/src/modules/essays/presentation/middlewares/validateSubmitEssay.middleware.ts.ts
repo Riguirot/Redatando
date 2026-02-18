4eee1e7c2ac2ec6f65ebb789b7caca5902fcac1e
Riguirot <felipe.porfirio77@gmail.com>
2026-02-17 21:37:55 -0300

    feat: atualizar scripts de build e validação de conteúdo

--- apps/backend/src/modules/essays/presentation/middlewares/validateSubmitEssay.middleware.ts.ts
+++ apps/backend/src/modules/essays/presentation/middlewares/validateSubmitEssay.middleware.ts.ts
@@ -1,20 +1,24 @@
 import { Request, Response, NextFunction } from "express";
 import { AppError } from "../../../shared/errors/AppError";
-import { CreateContentDTO } from "../../dtos/SubmitEssayDTO";
+import { SubmitEssayDTO } from "../../dtos/SubmitEssayDTO";
 
 export function validateCreateContent(
   req: Request,
   res: Response,
   next: NextFunction
 ) {
-  const body = req.body as Partial<CreateContentDTO>;
+  const body = req.body as Partial<SubmitEssayDTO>;
 
-  if (!body.title || typeof body.title !== "string") {
-    throw new AppError("Title is required", 400, "VALIDATION_ERROR");
+  if (!body.studentId || typeof body.studentId !== "string") {
+    throw new AppError("Student ID is required", 400, "VALIDATION_ERROR");
   }
 
-  if (!body.text || typeof body.text !== "string") {
-    throw new AppError("Text is required", 400, "VALIDATION_ERROR");
+  if (!body.theme || typeof body.theme !== "string") {
+    throw new AppError("Theme is required", 400, "VALIDATION_ERROR");
+  }
+
+  if (!body.file) {
+    throw new AppError("File is required", 400, "VALIDATION_ERROR");
   }
 
   next();
